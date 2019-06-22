447c7140254cd0f5229dd6214a63821818450ba7
Lulu Ngei <ngeilulu@gmail.com>
2019-06-22 21:15:15 +0300

    Events dashboard UI

---
+++ src/features/event/EventList/EventList.jsx
@@ -0,0 +1,16 @@
+import React, { Component, Fragment } from 'react'
+import EventListitem from './EventListitem';
+
+class EventList extends Component {
+    render() {
+        return (
+            <Fragment>
+                {this.props.events.map(event => (
+                    <EventListitem key={event.id} event={event} />
+                ))}
+            </Fragment>
+        )
+    }
+}
+
+export default EventList;
