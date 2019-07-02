76e89f4c58b62fe43f2a8d103f4bf0af9d72e6dc
Lulu Ngei <ngeilulu@gmail.com>
2019-07-02 21:26:52 +0300

    End of section 4

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Grid, Button } from 'semantic-ui-react';
 import EventList from '../EventList/EventList';
 import EventForm from '../EventForm/EventForm';
+import cuid from 'cuid';
 
 const eventsFromDashboard = [
   {
@@ -58,16 +59,35 @@ class EventDashboard extends Component {
     state ={
         events: eventsFromDashboard,
         isOpen : false
+    };
+
+    handleIsOpenToggle = () => {
+      this.setState(({isOpen}) => ({
+        isOpen: !isOpen
+      }))
+    }
+
+    handleCreateEvent = (newEvent) => {
+      newEvent.id = cuid();
+      newEvent.hostPhotoURL = '/assets/user.png';
+      this.setState(({events}) =>({
+        //set events to exisiting events and new events using a spread operator ...
+        // it takes the array of events and spreads them out
+        events: [...events, newEvent],
+        isOpen: false
+      }))
     }
     render() {
+      const {events, isOpen} = this.state;
         return (
             <Grid>
                 <Grid.Column width={10}>
-                    <EventList events = {this.state.events} />
+                    <EventList events = {events} />
                 </Grid.Column>
                 <Grid.Column width={6}>
-                    <Button positive content = 'Create Event' />
-                    <EventForm/>
+                    <Button onClick = {this.handleIsOpenToggle} positive content = 'Create Event' />
+                    {isOpen && <EventForm createEvent = {this.handleCreateEvent}
+                    cancelFormOpen={this.handleIsOpenToggle} />}               
                 </Grid.Column>
             </Grid>
         )
